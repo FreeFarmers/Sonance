1178697f6c6dae5b938a708b826e490e4f571e4d
Ahsan Minhas <ahsan.minhas.official@gmail.com>
2025-03-27 16:06:36 +0500

    Initial Commit

---
+++ Sonance/SonanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonanceApp.swift
+//  Sonance
+//
+//  Created by Ahsan Minhas on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SonanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
