209ddcdb3509d367791afb047cf47162946ed03e
Ahsan Minhas <ahsan.minhas.official@gmail.com>
2025-03-27 18:19:26 +0500

    Added Apple Watch Target

---
+++ Sonance Watch Watch App/Sonance_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sonance_WatchApp.swift
+//  Sonance Watch Watch App
+//
+//  Created by Ahsan Minhas on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Sonance_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
